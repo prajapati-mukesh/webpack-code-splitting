6908bf07f7f166758515e9c793af5bc61b266698
Mukesh Prajapati <mukesh.easternts@gmail.com>
2021-01-04 12:53:53 +0530

    Dynamic import

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import _ from 'lodash';
 
-function component() {
-    const element = document.createElement('div');
-   
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'Mukesh'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+function getComponent() {
+  const element = document.createElement('div');
+  return import('lodash')
+    .then(({ default: _ }) => {
+      const element = document.createElement('div');
+
+      element.innerHTML = _.join(['Hello', 'Mukesh'], ' ');
+ 
+  return element;
+      return element;
+    })
+    .catch((error) => 'An error occurred while loading the component');
+}
+getComponent().then((component) => {
+  document.body.appendChild(component);
+});
