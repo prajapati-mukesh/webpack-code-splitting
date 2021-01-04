b6db9486b77734c9bb443ce2dab1ba5d24fbe150
Mukesh Prajapati <mukesh.easternts@gmail.com>
2021-01-04 11:19:29 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+ 
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: {
+      import: './src/index.js',
+      dependOn: 'shared',
+    },
+    another: {
+      import: './src/another-module.js',
+      dependOn: 'shared',
+    },
+    shared: 'lodash',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+ optimization: {
+   runtimeChunk: 'single',
+ },
+};
